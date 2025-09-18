c63fdffa88245577d70124dce0bdfe09f956fd17
mohamed-khaledes <mohamedkhaelsayed@gmail.com>
2025-09-18 11:25:44 +0300

    add favorite and rate clicks

--- features/search/card.tsx
+++ features/search/card.tsx
@@ -1,9 +1,12 @@
+'use client'
 import Image from 'next/image';
-import { HeartIcon } from '@heroicons/react/outline';
-import { StarIcon } from '@heroicons/react/solid';
+import { StarIcon,HeartIcon } from '@heroicons/react/solid';
+import { useState } from 'react';
 
 type ListingCardProps = Omit<ListingCardItem, 'long' | 'lat'>;
 const UnitCard = ({unit}:{unit: ListingCardProps}) => {
+  const [isFavorite,setIsFavorite] = useState(false)
+  const [isRated,setIsRated] = useState(false)
   return (
     <div className='flex my-2 p-2 border-gray-200 border-b cursor-pointer pr-4 hover:opacity-80 hover:shadow-lg transition duration-200 ease-out first:border-t'>
       <div className='relative h-24 w-40 md:h-52 md:w-80 flex-shrink-0'>
@@ -17,14 +20,14 @@ const UnitCard = ({unit}:{unit: ListingCardProps}) => {
       <div className='flex flex-col flex-grow pl-5'>
         <div className='flex justify-between'>
           <p>{unit?.location}</p>
-          <HeartIcon className='h-7 cursor-pointer' />
+          <HeartIcon onClick={()=>setIsFavorite(!isFavorite)} className={`h-7 cursor-pointer ${isFavorite&&'text-red-500'}`} />
         </div>
         <h4 className='text-xl'>{unit?.title}</h4>
         <div className='border-b w-10 pt-2' />
         <p className='pt-2 text-sm text-gray-500 flex-grow'>{unit?.description}</p>
         <div className='flex justify-between items-end pt-5'>
           <p className='flex items-center'>
-            <StarIcon className='h-5 text-red-400' />
+            <StarIcon onClick={()=>setIsRated(!isRated)} className={`h-5 ${isRated&&'text-red-400'}`} />
             {unit?.star}
           </p>
           <div>
